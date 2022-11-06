cdd6178a1361d683b77780641f658d8019caf085
Denis Upadyshev <telec1979@yandex.ru>
2022-11-06 13:34:25 +0300

    Task 2 Solved

--- main.py
+++ main.py
@@ -10,6 +10,16 @@ class Student:
     def add_courses(self, course_name):
         self.finished_courses.append(course_name)
 
+    # здесь определяем функцию, которая выставляет оценки Lecturer
+    def rate_lecturer(self, lecturer, course, grade):
+        if isinstance(lecturer, Lecturer) and course in lecturer.courses_attached and course in self.courses_in_progress:
+            if course in lecturer.grades:
+                lecturer.grades[course] += [grade]
+            else:
+                lecturer.grades[course] = [grade]
+        else:
+            return 'Ошибка'
+
 
 class Mentor:
     def __init__(self, name, surname):
@@ -17,6 +27,18 @@ class Mentor:
         self.surname = surname
         self.courses_attached = []
 
+
+class Lecturer(Mentor):
+   
+    # надо определить здесь словарь, в котором хранятся оценки для Lecturer
+    def __init__(self, name, surname):
+        super().__init__(name, surname)
+        self.grades = {}
+
+
+class Reviewer(Mentor):
+    
+    # Пишем функцию выставления оценок только Reviewer
     def rate_hw(self, student, course, grade):
         if isinstance(student, Student) and course in self.courses_attached and course in student.courses_in_progress:
             if course in student.grades:
@@ -27,18 +49,9 @@ class Mentor:
             return 'Ошибка'
 
 
-
-class Lecturer(Mentor):
-    pass
-
-
-class Reviewer(Mentor):
-    pass
-
-
 best_student = Student('Ruoy', 'Eman', 'your_gender')
 best_student.finished_courses += ['Git']
-best_student.courses_in_progress += ['Python']
+best_student.courses_in_progress += ['Python', 'GIT']
 best_student.add_courses('GIT')
 #best_student.grades['Git'] = [10, 10, 10, 10, 10]
 #best_student.grades['Python'] = [10, 10]
@@ -48,13 +61,16 @@ best_student.add_courses('GIT')
 # print(best_student.grades)
 # print(best_student.finished_courses)
 
-cool_mentor = Mentor('Some', 'Buddy')
-cool_mentor.courses_attached += ['Python']
-cool_mentor.courses_attached += ['GIT']
-# print(cool_mentor.courses_attached)
+cool_lector = Lecturer('Some', 'Buddy')
+cool_reviewer = Reviewer('Some_r', 'Buddy_r')
+cool_reviewer.courses_attached += ['Python']
+cool_lector.courses_attached += ['GIT']
+print(cool_lector.courses_attached)
+best_student.rate_lecturer(cool_lector, 'GIT', 8) # выставляем оценку лектору
+print(cool_lector.grades) # проверяем
 
-cool_mentor.rate_hw(best_student, 'Python', 10)
-cool_mentor.rate_hw(best_student, 'Python', 20)
+cool_reviewer.rate_hw(best_student, 'Python', 10) # ставим оценку студенту
+# cool_mentor.rate_hw(best_student, 'Python', 20)
 # cool_mentor.rate_hw(cool_mentor, 'GIT', 22)
 # cool_mentor.rate_hw(best_student, 'GIT', 10)
 #
